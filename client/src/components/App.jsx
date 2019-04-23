2638e89bfa87c2e3f2eaa2c30316e8d526e18575
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-04-22 22:25:41 -0700

    Play around with styled components, make styled components elements in common folder

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,22 @@
 import React, { Component } from 'react';
+import { GlobalStyle, StyledP, WriteRevBox } from './elements';
+
+/*
+  This app needs tho display three components
+  and store all of the data for the reviews section
+  including:
+    -reviews
+    -stats data for reviews
+      -total reviews
+      -avg star rating 0_0 - 5_0
+      -avg fit 1_0 - 5_ 0
+      -avg length 1_0 - 5_ 0
+      -avg quality 1_0 - 5_ 0
+      -star rating breakdown
+      -size breakdown
+      -age breakdown
+      -height breakdown
+      */
 
 class App extends Component {
   constructor(props) {
@@ -11,9 +29,13 @@ class App extends Component {
   render() {
     const { string } = this.state;
     return (
-      <div>
-        {string}
-      </div>
+      <>
+        <GlobalStyle />
+        <div>
+          <StyledP>{string}</StyledP>
+        </div>
+        <WriteRevBox>Hello World</WriteRevBox>
+      </>
     );
   }
 }
